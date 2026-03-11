18dffcb5a4bd7b552627c1e64f47e7dbc79f677b
Deven Deming <dwd1021@usnh.edu>
2026-03-11 14:43:11 -0400

    Added bullet scene and script, added bullet sprite, added bullet collision with player and shield, added bullet rotation to bat.

--- scripts/bat.gd
+++ scripts/bat.gd
@@ -2,7 +2,26 @@ extends Area2D
 @onready var player: CharacterBody2D = $"../Player"
 @onready var shield: Area2D = $"../Player/Shield"
 
+const bullet_scene = preload("res://scenes/bullet.tscn")
+@onready var shoot_timer = $ShootTimer
+@onready var rotater: Node2D = $Rotater
+
+const rotation_speed = 100
+const shoot_timer_wait_time = 0.2
+const spawn_point_count = 4
+const radius = 20
+
 func _ready():
+	var step = 2 * PI / spawn_point_count
+	for i in range(spawn_point_count):
+		var spawn_point = Node2D.new()
+		var pos = Vector2(radius, 0).rotated(step * i)
+		spawn_point.position = pos
+		spawn_point.rotation = pos.angle()
+		rotater.add_child(spawn_point)
+
+	shoot_timer.wait_time = shoot_timer_wait_time
+	shoot_timer.start()
 	monitoring = true
 	visible = true
 
@@ -11,6 +30,17 @@ func _on_body_entered(body: Node2D) -> void:
 		body.hit()
 
 func _on_area_entered(area: Area2D) -> void:
-	print("Area entered:", area.name, " groups:", area.get_groups())
+	#print("Area entered:", area.name, " groups:", area.get_groups())
 	if (area.name == "Shield"):
 		queue_free()
+
+func _process(delta: float) -> void:
+	var new_rotation = rotater.rotation_degrees + rotation_speed * delta
+	rotater.rotation_degrees = fmod(new_rotation, 360)
+
+func _on_shoot_timer_timeout() -> void:
+	for s: Node2D in rotater.get_children():
+		var bullet = bullet_scene.instantiate()
+		get_tree().current_scene.add_child(bullet)
+		bullet.global_position = s.global_position
+		bullet.rotation = s.global_rotation
--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,1 +1,25 @@
 extends Area2D
+@onready var player: CharacterBody2D = $"../Player"
+@onready var shield: Area2D = $"../Player/Shield"
+
+const speed = 100
+
+func _ready():
+    monitoring = true
+    visible = true
+
+func _process(delta: float) -> void:
+    position += Vector2.RIGHT.rotated(rotation) * speed * delta
+
+func _on_kill_timer_timeout() -> void:
+    queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+    if body is CharacterBody2D:
+        body.hit()
+        queue_free()
+
+func _on_area_entered(area: Area2D) -> void:
+    #print("Area entered:", area.name, " groups:", area.get_groups())
+    if (area.name == "Shield"):
+        queue_free()
