c43904898c66fe29a3c510fefb2bcee4ea154d27
HiImDamien <fabianodamien@gmail.com>
2026-03-18 16:28:20 -0400

    Merged with devens_branch.

--- scripts/shield.gd
+++ scripts/shield.gd
@@ -5,33 +5,33 @@ extends Area2D
 var last_aim = Vector2.RIGHT
 
 func _ready():
-    monitoring = false
-    monitorable = false
-    visible = false
-    collision_polygon.disabled = true
+	monitoring = false
+	monitorable = false
+	visible = false
+	collision_polygon.disabled = true
 
 
 func activate():
-    monitoring = true
-    monitorable = true
-    visible = true
-    collision_polygon.disabled = false
+	monitoring = true
+	monitorable = true
+	visible = true
+	collision_polygon.disabled = false
 
 
 func deactivate():
-    monitoring = false
-    monitorable = false
-    visible = false
-    collision_polygon.disabled = true
+	monitoring = false
+	monitorable = false
+	visible = false
+	collision_polygon.disabled = true
 
 
 func _process(_delta):
-    if visible:
-        var aim = Input.get_vector("aim_left","aim_right","aim_up","aim_down")
-        if aim.length() > 0.2:
-            last_aim = aim
-        rotation = last_aim.angle()
+	if visible:
+		var aim = Input.get_vector("aim_left","aim_right","aim_up","aim_down")
+		if aim.length() > 0.2:
+			last_aim = aim
+		rotation = last_aim.angle()
 
 func _on_area_entered(area):
-    if area.is_in_group("enemy_bullet"):
-        area.queue_free()
+	if area.is_in_group("enemy_bullet"):
+		area.queue_free()
