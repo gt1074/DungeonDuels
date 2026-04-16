45b2551e81aa7cd26b6f38ce8186fe23f6e8766e
Dame <fabianodamien@gmail.com>
2026-04-15 22:40:00 -0400

    Small bug fixes
    Added collision to final battle walls. Made player not shoot their own shield.

--- scripts/player/player_bullet.gd
+++ scripts/player/player_bullet.gd
@@ -33,6 +33,8 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("shield") and area.is_active:
+		if area.get_parent() == shooter:
+			return
 		area.take_damage(damage)
 		queue_free()
 		return
